919772651fc81a5efc94789ea12687216c6448db
brian haltom <brianwh87@gmail.com>
2019-12-31 14:45:35 -0700

    Implemented comments, created model, added to views(show, index), set up associations, some styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'comments/create'
   devise_for :users
   root 'places#index'
   resources :places do
+    resources :comments, only: :create
     resources :photos, only: :create
   end
   get '/search' => 'places#search', :as => 'search_places'
